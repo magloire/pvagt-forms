95ac6187c345682bc5da722921948fdb51b1b19f
Magloire Sendegeya <magloire1@gmail.com>
2017-07-13 13:23:44 +0200

    Removed auto-generated comments

--- src/providers/rapport-service/rapport-service.ts
+++ src/providers/rapport-service/rapport-service.ts
@@ -2,12 +2,6 @@ import { Injectable } from '@angular/core';
 import { Http, HttpModule, Headers, RequestOptions, URLSearchParams } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the RapportServiceProvider provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular DI.
-*/
 @Injectable()
 export class RapportServiceProvider {
 
